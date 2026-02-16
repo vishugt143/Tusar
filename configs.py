97e4b89206221763d2a80ddcff27fc4284e1dfcd
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-02-16 11:24:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,7 +19,8 @@ class Config:
     # Posts to copy
     POSTS = [
         "https://t.me/forward_hack_lnx/126",
-        "https://t.me/forward_hack_lnx/127"
+        "https://t.me/forward_hack_lnx/127",
+        "https://t.me/forward_hack_lnx/128"
     ]
 
     # 🚫 ILLEGAL WORDS (BOT SIDE FILTER)
